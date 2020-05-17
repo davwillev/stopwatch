f1b3e5eef028f32edb45c634e68197b4901efa9a
Günther Rezniczek <g.rezniczek@gmail.com>
2020-05-18 00:08:44 +0200

    More typedefs

--- js/typedefs.js
+++ js/typedefs.js
@@ -19,6 +19,51 @@
  * @typedef StopwatchParams
  * @type {{
  *  elapsed: string
+ *  decimal?: string
+ *  digits: number
+ *  error?: string
  * }}
  */
 
+/**
+ * @typedef StopwatchData
+ * @type {{
+ *  id: string
+ *  $display: JQuery
+ *  params: StopwatchParams
+ *  running: boolean
+ *  elapsed: number
+ *  laps: LapInfo[]
+ *  clocktimer?: number
+ *  startTime?: Date
+ *  stopTime?: Date
+ *  lapStartTime?: Date
+ *  lapStopTime?: Date
+ * }}
+ */
+
+/**
+ * @typedef LapInfo
+ * @type {{
+ *  lapStartTime: Date
+ *  lapStopTime: Date
+ *  elapsed: number
+ *  isStop: boolean
+ * }}
+ */
+
+/**
+ * @typedef FormattedTime
+ * @type {{
+ *  time_ms: number
+ *  time_s: number
+ *  h: string
+ *  m: string
+ *  s: string
+ *  ms: string
+ *  ms_digits: number
+ *  ms_decimal : string
+ *  string: string
+ *  mm_ss: string
+ * }}
+ */
